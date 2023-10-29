dc4f0afef9003358d0b8836825db26d4797cd57f
Mahesh CHODA <choda.maheshbabu@gmail.com>
2023-10-29 20:06:50 +0530

    separate the child logic

--- src/lib/handleArticle.ts
+++ src/lib/handleArticle.ts
@@ -2,6 +2,15 @@ import { CollectionBeforeValidateHook } from "payload/types";
 import handleRichText from "./handleRichText";
 import handleImage from "../cloudImage/handleImage";
 
+// Function to handle child images
+async function handleChildImages(children, operation) {
+  for (const child of children) {
+    if (child.type === "upload" && child.value && child.value.id) {
+      await handleImage(child.value.id, operation);
+    }
+  }
+}
+
 const handleArticle: CollectionBeforeValidateHook = async ({
   data,
   req,
@@ -14,11 +23,7 @@ const handleArticle: CollectionBeforeValidateHook = async ({
 
     // Handling images within content children
     if (data?.content?.root?.children) {
-      for (const child of data.content.root.children) {
-        if (child.type === "upload" && child.value && child.value.id) {
-          await handleImage(child.value.id, operation); // Passing image data to handleImage
-        }
-      }
+      await handleChildImages(data.content.root.children, operation);
     }
   }
   return data; // Return data to either create or update a document with
